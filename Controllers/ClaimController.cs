20f08e5264f5f2a856b821d56f86044e4100d4cf
Dwayne Prins <ST10032544@vcconnect.edu.za>
2025-09-18 00:57:56 +0200

    Add project files.

---
+++ Controllers/ClaimController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMCSProject.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace CMCSProject.Controllers
+{
+	public class ClaimController : Controller
+	{
+
+		private static Claim currentClaim = null;
+		public IActionResult Index()
+		{
+			return View(currentClaim);
+		}
+
+		[HttpPost]
+		public IActionResult Submit(Claim claim, IFormFile document)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View("Index", claim);
+			}
+
+			if (document != null)
+			{
+				var fileName = Path.GetFileName(document.FileName);
+				claim.UploadedDocument = fileName;	
+			}
+
+			claim.Status = "Submitted";
+			currentClaim = claim;
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Verify()
+		{
+			if (currentClaim != null)
+			{
+				currentClaim.Status = "Verfied by Coordinator";
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Approve()
+		{
+			if (currentClaim != null)
+			{
+				currentClaim.Status = "Approved by Manager";
+			}
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Models/Claim.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMCSProject.Models
+{
+	public class Claim
+	{
+		public int Id { get; set; }	
+
+		[Required]
+		[Display(Name ="Lecturer Name")]
+		public string LecturerName { get; set; }
+
+		[Required]
+		[Display(Name ="Hours Worked")]
+		public string HoursWorked { get; set; }
+		
+		[Required]
+		[Display(Name ="Hourly Rate")]
+		public string HourlyRate { get; set; }
+
+		[Display(Name = "Status")]
+		public string Status { get; set; } = "Pending";
+
+		[Display(Name ="Uploaded Document")]
+		public string UploadedDocument { get; set; }
+	}
+}
